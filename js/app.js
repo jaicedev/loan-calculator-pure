3e8339e2d9f328a6726622023cf729f32261e648
jaicedev <jaicedev@gmail.com>
2020-08-26 15:53:15 -0400

    Added ChartJS Integration, Set up some framework for Amortization Table

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ function retrieveLoanTermLength() {
 function showMonthlyPayment(length, value, interest) {
   let payment = monthlyPayment(length, value, interest);
   $("#js-calculated-payment").text("$" + payment);
-  $("#js-principal").text("$" + parseFloat(value).toFixed(2));
+  $("#js-principal").text("$" + parseFloat(value));
   return payment;
 }
 
@@ -25,7 +25,7 @@ function showInterestPaid(length, value, interest) {
   let payment = monthlyPayment(length, value, interest);
   let interestPaid = payment * length - value;
   $("#js-interest-paid").text("$" + parseFloat(interestPaid).toFixed(2));
-  return interestPaid;
+  return parseFloat(interestPaid).toFixed(2);
 }
 
 function showTotalCost(length, value, interest) {
@@ -35,6 +35,7 @@ function showTotalCost(length, value, interest) {
     .toFixed(2)
     .toString();
   $("#js-total-cost").text("$" + totalCost);
+  return totalCost;
 }
 
 function showCalculation() {
@@ -50,9 +51,55 @@ function showCalculation() {
       $("#invalid-input").hide();
       showInterestPaid(loanLength, loanValue, loanInterest);
       showMonthlyPayment(loanLength, loanValue, loanInterest);
-      showTotalCost(loanLength, loanValue, loanInterest);
+      //   showTotalCost(loanLength, loanValue, loanInterest);
+      updateChart();
     }
   });
 }
 
+function advancedView() {
+  $("#js-advanced-table").hide();
+  $("#js-advanced").on("click", function () {
+    $("#js-advanced").hide();
+    $("#js-advanced-table").show();
+    createChart();
+  });
+}
+
+function updateChart() {
+  if ($("#myChart").hasClass("chartjs-render-monitor")) {
+    createChart();
+  } else {
+    console.log("No Chart Created");
+  }
+}
+
+function createChart() {
+  $("#myChart").text("");
+  let loanLength = retrieveLoanTermLength();
+  let loanValue = $("#js-loan-value").val();
+  let loanInterest = $("#js-interest").val();
+  var chartInsertion = document.getElementById("myChart").getContext("2d");
+  var paymentChart = new Chart(chartInsertion, {
+    type: "doughnut",
+    data: {
+      labels: ["Principal", "Interest"],
+      datasets: [
+        {
+          label: "Payments",
+          data: [
+            loanValue,
+            showInterestPaid(loanLength, loanValue, loanInterest),
+          ],
+          backgroundColor: ["#f7b167d2", "#f4845f"],
+          borderColor: ["#f7b267", "#f7b267"],
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {},
+  });
+}
+
+$(advancedView);
 $(showCalculation);
